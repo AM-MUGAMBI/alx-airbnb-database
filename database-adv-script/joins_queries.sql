6a3802fbffe93ce48e95700f0ddc3af1baccc6ee
AM-MUGAMBI <amugambi370@gmail.com>
2025-09-06 18:36:41 +0300

    task 0

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,32 @@
--- LEFT JOIN part
+-- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT 
+    b.booking_id,
+    b.booking_date,
+    b.property_id,
+    u.user_id,
+    u.name AS user_name,
+    u.email
+FROM 
+    bookings b
+INNER JOIN 
+    users u ON b.user_id = u.user_id;
+
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    p.property_id,
+    p.property_name,
+    r.review_id,
+    r.rating,
+    r.comment
+FROM 
+    properties p
+LEFT JOIN 
+    reviews r ON p.property_id = r.property_id;
+
+
+-- 3. FULL OUTER JOIN (emulated in MySQL): Retrieve all users and all bookings, even if unmatched
+-- Part 1: Users LEFT JOIN Bookings
 SELECT 
     u.user_id,
     u.name AS user_name,
@@ -13,7 +41,7 @@ LEFT JOIN
 
 UNION
 
--- RIGHT JOIN part
+-- Part 2: Users RIGHT JOIN Bookings
 SELECT 
     u.user_id,
     u.name AS user_name,
@@ -25,4 +53,3 @@ FROM
     users u
 RIGHT JOIN 
     bookings b ON u.user_id = b.user_id;
-
